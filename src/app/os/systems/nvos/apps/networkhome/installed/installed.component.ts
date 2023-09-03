2f7cdb0e2fad272319893859ec8030715bdcf6d1
MasT3r1k <master0123@centrum.cz>
2023-09-03 15:32:53 +0200

    Něco málo do NetworkHome

--- src/app/os/systems/nvos/apps/networkhome/installed/installed.component.ts
+++ src/app/os/systems/nvos/apps/networkhome/installed/installed.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NProcesses } from '../../../Main/Processes';
 import { ApplicationDatabase } from '../NetworkHome';
 
+type sections = "main" | "moreInfo";
+
 @Component({
   selector: 'app-installed',
   templateUrl: './installed.component.html',
@@ -9,7 +11,36 @@ import { ApplicationDatabase } from '../NetworkHome';
 })
 export class InstalledComponent {
   public apps = NProcesses.getInstalledProcesses();
+  public declare selectedApp: NProcesses.Process;
+  private section = "main";
   public getAppIcon(process: string): string {
     return ApplicationDatabase.filter(_ => _.name === process)[0].icon;
   }
+
+  public setSection(section: sections): void {
+    this.section = section;
+  }
+
+  public getSection(): string {
+    return this.section;
+  }
+
+  public setSelectedApp(app: NProcesses.Process | void):void {
+    if (!app) return;
+    this.selectedApp = app;
+  }
+
+  public getSelectedApp(): NProcesses.Process | undefined {
+    return this.selectedApp;
+  }
+
+  public showApp(app: string): void {
+    this.setSection('moreInfo');
+    this.setSelectedApp(this.getProcessByName(app));
+  }
+  
+  public getProcessByName(name: string): NProcesses.Process | void {
+    if (NProcesses.getProcess(name) !== undefined) return NProcesses.getProcess(name);
+  }
+
 }
